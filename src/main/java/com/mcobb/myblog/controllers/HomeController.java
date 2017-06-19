4fe1ddcb90e8b2ea41c72d941319874b2bf988e2
Michael cobb <michael.cobb@jcrew.com>
2017-06-19 15:37:48 -0400

    added comments

--- src/main/java/com/mcobb/myblog/controllers/HomeController.java
+++ src/main/java/com/mcobb/myblog/controllers/HomeController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public  String hello(Model model){
+        /*
+         This is just a skeleton this will change and morph with
+         the project.
+         */
         model.addAttribute("name", "World");
         return "home";
     }
